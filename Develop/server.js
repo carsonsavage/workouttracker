d71baee64beec574074bdc882bdbb1cc0545dc02
Carson Savage <carsonesavage@gmail.com>
2021-01-21 23:56:45 -0600

    server updating

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const path = require("path");
-
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,30 +13,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/workout',
-    {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-    }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useFindAndModify: false });
 
-
-require("./routes/api-routes.js")(app);
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "/public/index.html"));
-});
-
-app.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname + "/public/exercise.html"));
-});
-
-app.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname + "/public/stats.html"));
-});
+app.use(require("./routes/api.js"));
+app.use(require("./routes/views.js"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
